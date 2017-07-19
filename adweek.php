bf6ce878acfd3bc5c8015b41400f330775758c65
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-07-18 23:54:49 -0400

    hook into the_content

--- adweek.php
+++ adweek.php
@@ -5,9 +5,25 @@ Author: Joe Jalbert
 Description: Adds a 'recent post' box to the bottom of every article, showing the newest story from the site
 */
  ?>
+<?php
+function add_my_stuff($content){
+  if (is_single()){
+    $content .= "aside>
+      <div class='thumb'>
+        <img src='image.png'></img>
+      </div>
+      <h2 class='category'><a>ADFREAK</a></h2>
+      <div class='title'>Toyota's Very Strange New Cinema Ads Will Have Moviegoers Doign a Double-Take</div>
+      <div class='info'>By David Gianatasio 12 Minutes ago</div>";
+  }
+  return $content;
+}
 
+add_filter ('the_content', 'add_my_stuff', 0);
 
-<link rel="stylesheet" type="text/css" href="style.css">
+ ?>
+
+<!-- <link rel="stylesheet" type="text/css" href="style.css">
 <aside>
   <div class='thumb'>
     <img src='image.png'></img>
@@ -15,4 +31,4 @@ Description: Adds a 'recent post' box to the bottom of every article, showing th
   <h2 class='category'><a>ADFREAK</a></h2>
   <div class='title'>Toyota's Very Strange New Cinema Ads Will Have Moviegoers Doign a Double-Take</div>
   <div class='info'>By David Gianatasio 12 Minutes ago</div>
-</aside>
+</aside> -->

00e8ecfacee418bd0c34a6a45acd0e38e0b36975
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-07-19 16:37:50 -0400

    clean directory

--- post-box.php
+++ post-box.php
@@ -12,29 +12,27 @@ function add_post_box($content) {
     wp_enqueue_style( 'post_box');
 
     $latest = wp_get_recent_posts(array('numberposts' => '1'))[0];
-    $latestID = $latest["ID"];
-    $category = get_the_category($latestID)[0]->name;
-    $catID = get_the_category($latestID)[0]->cat_ID;
-    $catLink = get_category_link($catID);
-    $post_time = get_post_time('U', false, $latestID, false);
+    $latest_ID = $latest["ID"];
+    $category = get_the_category($latest_ID)[0]->name;
+    $cat_ID = get_the_category($latest_ID)[0]->cat_ID;
+    $cat_link = get_category_link($cat_ID);
+    $post_time = get_post_time('U', false, $latest_ID, false);
     $author_id = $latest["post_author"];
-    $authorLink = get_author_posts_url($author_id);
+    $author_link = get_author_posts_url($author_id);
     $author_name = get_the_author_meta("first_name",$author_id) . ' ' . get_the_author_meta("last_name",$author_id);
-    $thumb = get_the_post_thumbnail($latestID, "medium");
+    $thumb = get_the_post_thumbnail($latest_ID, "medium");
     $time_ago = human_time_diff($post_time, current_time('U')) . " ago";
-    $latestLink = get_post_permalink($latestID);
-
+    $latest_link = get_post_permalink($latest_ID);
 
     $content .= '
       <div class="post-box">
-        <div class="thumb"><a href=' . $latestLink . '>' . $thumb . '</a></div>
+        <div class="thumb"><a href=' . $latest_link . '>' . $thumb . '</a></div>
         <div class="post-info">
-          <h2 class="category"><a href=' . $catLink . '>' . $category . '</a><span class="mobile-time"> | ' . $time_ago . '</span></h2>
-          <div class="headline"><a href=' . $latestLink . '>' . $latest["post_title"] . '</a></div>
-          <div class="desktop-info">By <span class="author"><a href=' . $authorLink . '>' . $author_name . '</a></span> ' . $time_ago . '</div>
+          <h2 class="category"><a href=' . $cat_link . '>' . $category . '</a><span class="mobile-time"> | ' . $time_ago . '</span></h2>
+          <div class="headline"><a href=' . $latest_link . '>' . $latest["post_title"] . '</a></div>
+          <div class="desktop-info">By <span class="author"><a href=' . $author_link . '>' . $author_name . '</a></span> ' . $time_ago . '</div>
         </div>
       </div>
-
     ';
   }
   return $content;

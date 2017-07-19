f50870cf7947d331dd59a77e103136f3c4b85815
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2017-07-19 10:03:59 -0400

    get the image

--- adweek.php
+++ adweek.php
@@ -6,30 +6,33 @@ Description: Adds a 'recent post' box to the bottom of every article, showing th
 */
  ?>
 <?php
-function add_my_stuff($content){
+function add_post_box($content){
   if (is_single()){
 
     $latest = wp_get_recent_posts(array('numberposts' => '1'))[0];
     $latestID = $latest["ID"];
     $category = get_the_category($latestID)[0]->name;
     $post_time = get_post_time('U', false, $latestID, false);
-    var_dump($post_time);
     $author_id = $latest["post_author"];
     $author_name = get_the_author_meta("first_name",$author_id) . ' ' . get_the_author_meta("last_name",$author_id);
+    $thumb = get_the_post_thumbnail($latestID);
+    $time_ago = human_time_diff($post_time, current_time('U')) . " ago";
 
-    $content .= "<aside>
-      <div class='thumb'>
-        <img src='image.png'></img>
-      </div>
-      <h2 class='category'><a>$category</a></h2>
-      <div class='title'>'" . $latest["post_title"] . "</div>
-      <div class='info'>By $author_name " . human_time_diff($post_time, current_time('U')). " ago</div>";
-      echo $post_date . ' ' . current_time('Y-m-d H:i:s');
+    $content .= "
+      <aside>
+        <div class='thumb'>
+          <img src='image.png'>$thumb</img>
+        </div>
+        <h2 class='category'><a>$category</a><div class='mobileTime'>| $time_ago</div></h2>
+        <div class='title'>" . $latest["post_title"] . "</div>
+        <div class='info'>By $author_name $time_ago</div>
+      </aside>
+    ";
   }
   return $content;
 }
 
-add_filter ('the_content', 'add_my_stuff', 0);
+add_filter ('the_content', 'add_post_box', 0);
 
  ?>
 
